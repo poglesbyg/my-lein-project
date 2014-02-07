9bef2edd7f1d34b734625939291d5303cfd172d3
poglesby <poglesbyg@gmail.com>
2014-02-07 08:02:05 +0100

    initial commit

---
+++ test/my_lein_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns my-lein-project.core-test
+  (:require [clojure.test :refer :all]
+            [my-lein-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
